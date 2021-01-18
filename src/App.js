3d94b5af3e2632766fb93e17d972e3b2b40ccb0e
Vitor Nicolau <vitor.webdeveloper@gmail.com>
2021-01-18 22:30:01 +0000

    Change H1 element

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
     return (
         <div className="App">
             <header>
-                <h1>Vitor's Todo List</h1>
+                <h1>Todo List</h1>
             </header>
             <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus} />
             <TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={todos} />
